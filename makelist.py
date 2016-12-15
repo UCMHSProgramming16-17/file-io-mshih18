15bb9cafd4868993cffbe600f8c2ff55220084a0
Matthew Shih <mshih@ucvts.org>
2016-12-15 14:42:54 +0000

    Added comments to makelist.py

--- makelist.py
+++ makelist.py
@@ -1,6 +1,11 @@
+# open a new file named 'list.txt'
 file = open('list.txt','w')
 
+# loop ten times
 for x in range(10):
-    file.write(str(x + 1) + ". " + input("What do you want to add to the list? ") +  '\n')
     
+    # get user input, write it to file, create new line
+    file.write(str(x + 1) + ". " + input("What do you want to add to the list? ") +  '\n')
+  
+# close the file 
 file.close()
